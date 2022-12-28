86fdbb8b6efc22e5fb13bed354817aad52e1c67b
Ankara.mg <solroman44@gmail.com>
2022-12-28 19:01:30 -0300

    config handlebars

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.engine('.hbs', engine({
     layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
     defaultLayout: "main",
     extname: '.hbs',
 }));
