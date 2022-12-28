3d99db676085aa9593122031de7d79fde64ca022
Ankara.mg <solroman44@gmail.com>
2022-12-28 19:00:19 -0300

    config handlebars

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 import express from 'express';
 import indexRoutes from './routes/index.routes';
+import path from 'path';
+import { engine } from 'express-handlebars';
 
 const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.engine('.hbs', engine({
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    defaultLayout: "main",
+    extname: '.hbs',
+}));
+
+app.set('view engine', '.hbs')
 
 app.use(indexRoutes);
 
